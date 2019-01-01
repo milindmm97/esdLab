9f9170c38f5f1ec15c5253e60aac3a0c3372f479
Milind M <milindmmanoj@outlook.com>
2019-01-01 12:09:41 +0530

    Update THUMBseriesSum.asm

--- THUMBseriesSum.asm
+++ THUMBseriesSum.asm
@@ -1,33 +1,28 @@
-          NAME main 
-          PUBLIC __iar_program_start 
-          SECTION .datas:CONST (2)
-data dcd 0x05,0x3,0x0F,0x02
-data_end dcd 0x00
-data_length dcd (data_end - data)/4
-          SECTION .intvec:CODE:NOROOT (2)
+              NAME main
+    PUBLIC __iar_program_start
+    SECTION datas:CONST(2)
+num
+    dc32 0x0A
+sum
+    ds32 1
+    SECTION .intvec:CODE:NOROOT (2)
 __iar_program_start
-main 
-          ldr r0,=data
-          ldr r1,data                   ;r1 holds the smallest number
-          ldr r2,data                   ;r2 hold the largest number
-          ldr r3,data_length            
-          sub r3,r3,#1                  ;r3 is the counter
-          ldr r4,=thumbcode 
-          mov lr,pc 
-          bx r4
-stop      b stop 
-  code16
+main
+    ldr r6,num
+    ldr r0,=thumbcode
+    mov lr,pc
+    bx r0
+    str r1,sum
+stop b stop
+    code16
 thumbcode
-loop      add r0,#4
-          ldr r5,[r0]
-          cmp r5,r1
-          bpl cont
-          mov r1,r5
-cont      cmp r5,r2
-          bmi check
-          mov r2,r5
-check     sub r3,#1
-          cmp r3,#0
-          bne loop 
-          bx lr 
-          end
+    mov r4,r6
+    mov r1,#0x00
+loop mul r6,r4
+    add r1,r1,r6
+    sub r4,#1
+    mov r6,r4
+    bne loop
+here
+    bx lr
+  END
